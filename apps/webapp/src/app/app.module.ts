db9e1bbc812abf059e9837a18ba81697fe29af92
Satish Chilkaka <73182406+Satishchilkaka@users.noreply.github.com>
2021-06-25 23:08:59 -0400

    Update app modules

--- apps/webapp/src/app/app.module.ts
+++ apps/webapp/src/app/app.module.ts
@@ -4,9 +4,12 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { RouterModule } from "@angular/router";
+import { ServerComponent } from "./server/server.component";
+import { TestComponent } from './test/test.component';
+
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ServerComponent, TestComponent],
   imports: [
     BrowserModule,
     FormsModule,
